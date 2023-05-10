cdbd1f22b8deca3872dbb9c561aedd9585480664
spbreeva <sonya.breeva@gmail.com>
2023-05-10 19:57:51 +0300

    Добавила функцию

--- 18_check_digit.py
+++ 18_check_digit.py
@@ -13,3 +13,26 @@
 #     Есть ли цифра 4 в числе 67845123654 между индексами 0 и 0? True
 #     Есть ли цифра 1 в числе 9999999999 между индексами 2 и 5? False
 
+
+def find_number_in_range(number, digit, index1, index2):
+    if index1 == 0 and index2 == 0:
+        digit2 = digit[:]
+    elif index1 == 0:
+        digit2 = digit[:index2]
+    elif index2 == 0: 
+        digit2 = digit[:index1]
+    elif index1 > index2:
+        digit2 = digit[index1:index2]
+    else: 
+        digit2 = digit[index2:index1]
+    
+    counter_true = 0
+    for i in range(len(digit2)):
+        if int(digit2[i]) == number:
+            counter_true += 1 
+        i += 1 
+
+    if counter_true > 0:
+        return True
+    else:
+        return False
